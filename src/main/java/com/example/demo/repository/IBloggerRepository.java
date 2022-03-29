1c1331d9f848d90dab0964e99f02e7a05e5795dc
Pranay Shanigarapu <47024307+rajpranay06@users.noreply.github.com>
2022-03-29 12:47:09 +0530

    Update IBloggerRepository.java

--- src/main/java/com/example/demo/repository/IBloggerRepository.java
+++ src/main/java/com/example/demo/repository/IBloggerRepository.java
@@ -19,7 +19,7 @@ import com.example.demo.bean.Blogger;
 public interface IBloggerRepository extends JpaRepository<Blogger, Integer> {
 
 	@Query(value="select b.* from blogger b join comment c on b.user_id=c.user_id where c.comment_id=:commentId",nativeQuery=true)
-	Blogger getBloggerByCommentId(@Param("commentId") int commentId);
+	public Blogger getBloggerByCommentId(@Param("commentId") int commentId);
 
 	@Query(value = "SELECT b.* from blogger b join blogger_and_communities bc on b.user_id = bc.user_id join community c on bc.community_id = c.community_id where c.community_id = :communityId", nativeQuery = true)
 	public List<Blogger> viewBloggerListByCommunityId(@Param("communityId") int communityId);
